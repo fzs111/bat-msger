32c0fb6d9269c73144ebfa424184d910c1dacb28
FZs <39064892+fzs111@users.noreply.github.com>
2025-09-15 15:22:16 +0200

    Add files via upload

---
+++ messenger.bat
@@ -0,0 +1,10 @@
+@echo off
+chcp 65001 >nul
+echo Feladó: %computername%
+set /p "computer=Címzett: "
+:a
+set "msg="
+set /p "msg=%computer%> "
+if not [%msg%]==[] set msg="%msg%"
+msg /server:%computer% hallgato %msg%
+goto a
